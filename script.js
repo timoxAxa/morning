480d1a03a79e72f94fac166dede0aefe62668c48
Timoxa-Web <119561170+timoxAxa@users.noreply.github.com>
2022-12-22 08:22:12 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+// let arr = ['Mukhlkisa', "Ilon", 'Darya', 'Devid', 'islom', 'Shakh', 'Timoxa', 'Ravshan', 'Ibragim', "Далер", 'daler', "Aлександр"]
+
+// let sorted = arr.sort((a, b) => a.length - b.length)
+// let aaa = sorted.at(-1)
+// console.log(aaa);
+
+// let a = 7
+// let b;
+// const someFunc = () => {
+//     let b;
+//     a = null;
+//     b = 5;
+//     console.log(b);
+// }
+// someFunc();
+// console.log(a);
+// console.log(b);
+
+
+// let name = prompt('Какое официальное название?')
+// if (name = 'ecmascript') {
+//     alert('верно');
+// }else('неверно');
+
+let love = ['C', 'добрым', 'утречком!']
+alert(love[0])
+alert(love[1])
+alert(love[2])
